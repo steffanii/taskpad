fa0ec4b21e12f8bddce9b23dcf35378929859a31
steffanii <stephanienwosumadueke@gmail.com>
2024-10-29 13:37:07 +0100

    domain updates

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -30,9 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DJANGO_DEBUG")
+DEBUG = env("DJANGO_DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskpad.render.app"]
+CSRF_TRUSTED_ORIGIN= ['taskpad.render.app']
 
 
 # Application definition
