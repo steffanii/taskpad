44101690708ca84626e91f32370d89b0611f3fae
steffanii <stephanienwosumadueke@gmail.com>
2024-10-29 19:37:06 +0100

    domain correction

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://taskpad-de3x.onrender.com"]
-CSRF_TRUSTED_ORIGINS= ['https://taskpad-de3x.onrender.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
